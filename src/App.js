be650fd913a968c78b437213ba9165b7b5b13468
Pavel <>
2022-12-28 22:43:58 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rveact
         </a>
       </header>
     </div>
